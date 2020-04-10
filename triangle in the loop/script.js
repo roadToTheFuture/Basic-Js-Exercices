a54ee06dd0fc0c6f881ffdd2c6c12e03d8ffa214
roadToTheFuture <63185603+roadToTheFuture@users.noreply.github.com>
2020-04-10 18:11:01 +0300

    Did task: triangle in the loop

---
+++ triangle in the loop/script.js
@@ -0,0 +1,15 @@
+// alert('look in the console');
+let triangleStar = ' ';
+let x = 1;
+
+// for (let x = 1; x <= 7; x++){
+// 		while(triangleStar.length == x){
+// 			triangleStar += '*';
+// 		}
+// 		document.write(triangleStar);
+// 		document.write('</br>');
+// 		console.log(triangleStar);
+// }
+
+for (triangleStar = '*'; triangleStar.length <= 7; triangleStar += '*')
+	console.log(triangleStar);
